ceb2c31e0a12c84cdd40d149f24a79efc8a5b9c5
Guillaume Quéré <none>
2019-03-06 15:07:27 +0100

    [fix] Bunch of problems

--- check_sudoers.sh
+++ check_sudoers.sh
@@ -46,7 +46,8 @@ grep '\$' /etc/shadow | cut -c -40
 subtitle "Users with empty passwords"
 while read -r line
 do
-    if [ -z $(echo "$line" | cut -d':' -f2) ]
+    pass=$(echo "$line" | cut -d':' -f2)
+    if [ -z "$pass" ]
     then
         echo $line
     fi
@@ -55,7 +56,7 @@ done < /etc/shadow
 
 # SUDO CONFIGURATION ###########################################################
 title "$(ls -la /etc/sudoers)" "$(md5sum /etc/sudoers)"
-cat /etc/sudoers
+grep -v '^#' /etc/sudoers | grep -v '^$'
 
 title "$(ls -lad /etc/sudoers.d/)"
 
@@ -66,7 +67,7 @@ then
         file_permissions=$(ls -la "$file")
         file_hash=$(md5sum "$file")
         subtitle "$file_permissions" "$file_hash"
-        cat "$file"
+        grep -v '^#' "$file" | grep -v '^$'
     done
 else
     echo "Directory is empty"
@@ -90,24 +91,36 @@ do
     then
         continue
     fi
-    subtitle "User $user"
 
-    while read -r path
+    # split line contents into an array, it could be pretty pathological, i.e.:
+    #     (root) path1, (root) path2
+    #     path3
+    paths_array=()
+    for line in $paths
     do
-        if [ "$path" == "ALL" ]
-        then
-            echo "ALL"
-            continue
-        fi
+        line=$(echo "$line" | sed -e 's/,//g')
+        for elem in $line
+        do
+            if [[ "$elem" =~ ^/ ]]
+            then
+                paths_array+=("$elem")
+            fi
+        done
+    done
 
+    user_groups=$(groups "$user")
+    subtitle "User ""$user" "Groups ""$user_groups"
+
+    for path in "${paths_array[@]}"
+    do
         if [ -e "$path" ]
         then
             ls -la "$path"
 
-            # file is a symlink
+            # file is a symlink, attempt to dereference it
             if [ -L "$path" ]
             then
-                target=$(readlink "$path")
+                target=$(readlink -f "$path")
                 if [ -e "$target" ]
                 then
                     ls -la "$target"
@@ -115,12 +128,6 @@ do
             fi
         else
             echo "$path does not exist"
-
-            # check that path starts with '/'
-            if [[ ! $path =~ ^/ ]]
-            then
-                continue
-            fi
 
             # find last existing path
             loop_control=0
@@ -139,7 +146,7 @@ do
                 fi
             done
         fi
-    done <<< $paths
+    done
 done
 
 title "Done"
